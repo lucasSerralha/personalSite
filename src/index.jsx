8634fae55a0538eefd86549c3f65363a2ed92e44
lucasSerralha <lucas.serralha@gmail.com>
2022-02-04 15:12:02 -0300

    feature: add html static

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,5 @@
-import React from 'react';
+import { render } from 'react-dom';
 import { App } from './App';
+
+// eslint-disable-next-line react/react-in-jsx-scope
+render(<App />, document.getElementById('root'));
