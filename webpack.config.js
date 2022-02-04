78406ae49bb5a5daf645fa4d9fbc1e697799c0c2
lucasSerralha <lucas.serralha@gmail.com>
2022-02-04 15:25:43 -0300

    feature: webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,15 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  devServer: {
+    static: path.resolve(__dirname, 'public'),
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html'),
     }),
   ],
+
   module: {
     rules: [
       {
